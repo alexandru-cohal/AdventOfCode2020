f57ae4953e20c2cbd58048623d85479a6b17dd5a
Alexandru Cohal <alexandru.cohal@yahoo.com>
2020-12-15 23:00:42 +0100

    Solution for day 15 - puzzle 1

--- AdventOfCode2020/AdventOfCode2020.cpp
+++ AdventOfCode2020/AdventOfCode2020.cpp
@@ -5,11 +5,11 @@
 //      - Change the header file (e.g. #include "day3/day3puzzle2.h")
 //      - Change the function called from the 'main' function (e.g. mainDay3Puzzle2();)
 
-#include "day14/day14puzzle2.h"
+#include "day15/day15puzzle1.h"
 
 int main()
 {
-    mainDay14Puzzle2();
+    mainDay15Puzzle1();
 
     return 0;
 }
---
+++ AdventOfCode2020/day15/day15puzzle1.h
@@ -0,0 +1,16 @@
+#ifndef DAY15PUZZLE1_H
+#define DAY15PUZZLE1_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+void mainDay15Puzzle1();
+void readInputsDay15Puzzle1(vector<int>& starting);
+int solveDay15Puzzle1(vector<int> starting);
+
+#endif
