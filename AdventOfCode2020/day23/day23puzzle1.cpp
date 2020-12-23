2520a5ac900de67c2091179791354a2f8f7841de
Alexandru Cohal <alexandru.cohal@yahoo.com>
2020-12-23 21:36:52 +0100

    Improvement  of the solution for day 23 - puzzle 1

--- AdventOfCode2020/day23/day23puzzle1.cpp
+++ AdventOfCode2020/day23/day23puzzle1.cpp
@@ -29,16 +29,15 @@ string solveDay23Puzzle1(vector<int> labels)
 	// The index of the current cup is always kept on 1
 	// The indices of the picked-up cups are always 2, 3 and 4
 
+	// Create a set with the labels within the destination cup is searched for
+	set<int> labelsToSearch;
+	for (auto itLabels = labels.begin() + 4; itLabels != labels.end(); itLabels++)
+	{
+		labelsToSearch.insert(*itLabels);
+	}
 	// Iterate over each move
 	for (int indexMove = 1; indexMove <= NUMMOVES; indexMove++)
 	{
-		// Create a set with the labels within the destination cup is searched for
-		set<int> labelsToSearch;
-		for (auto itLabels = labels.begin() + 4; itLabels != labels.end(); itLabels++)
-		{
-			labelsToSearch.insert(*itLabels);
-		}
-
 		// Search for the destination cup and change its label is not found
 		int labelToFind = labels[0] - 1;
 		while (labelsToSearch.find(labelToFind) == labelsToSearch.end())
@@ -50,12 +49,24 @@ string solveDay23Puzzle1(vector<int> labels)
 			}
 		}
 
+		// Update the set
+		labelsToSearch.insert(labels[0]);
+		labelsToSearch.insert(labels[1]);
+		labelsToSearch.insert(labels[2]);
+		labelsToSearch.insert(labels[3]);
+
 		// Move the picked-up cups
 		labels.insert(find(labels.begin() + 4, labels.end(), labelToFind) + 1, labels.begin() + 1, labels.begin() + 4);
 		// Add the current label to the end 
 		labels.push_back(labels[0]);
 		// Remove the current label and the picked-up cups (they are duplicated now)
 		labels.erase(labels.begin(), labels.begin() + 4);
+
+		// Update the set
+		labelsToSearch.erase(labels[0]);
+		labelsToSearch.erase(labels[1]);
+		labelsToSearch.erase(labels[2]);
+		labelsToSearch.erase(labels[3]);
 	}
 
 	// Create the string from the current cups configuration such that to have cup 1 on the first position
